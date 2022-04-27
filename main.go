a7c7e1f5c1cb9bee7f52cbcb0c63957a7d7bd591
Pedraam Nikzad <pnikzad@zuora.com>
2022-04-27 00:00:59 -0700

    main.go cleanup

--- main.go
+++ main.go
@@ -1,28 +1,24 @@
 package main
 
 import (
-	"os"
 
 	"github.com/gin-gonic/gin"
-	"gitlab.com/pragmaticreviews/golang-gin-poc/api"
-	"gitlab.com/pragmaticreviews/golang-gin-poc/controller"
-	"gitlab.com/pragmaticreviews/golang-gin-poc/docs" // Swagger generated files
-	"gitlab.com/pragmaticreviews/golang-gin-poc/middlewares"
-	"gitlab.com/pragmaticreviews/golang-gin-poc/repository"
-	"gitlab.com/pragmaticreviews/golang-gin-poc/service"
+	"github.com/pedraamy/gin-api/api"
+	"github.com/pedraamy/gin-api/controller"
+	"github.com/pedraamy/gin-api/repository"
+	"github.com/pedraany/gin-api/api"
+	"github.com/pedraany/gin-api/controller"
+	"github.com/pedraany/gin-api/middlewares"
+	"github.com/pedraany/gin-api/respository"
 
 	swaggerFiles "github.com/swaggo/files"     // swagger embed files
 	ginSwagger "github.com/swaggo/gin-swagger" // gin-swagger middleware
 )
 
 var (
-	videoRepository repository.VideoRepository = repository.NewVideoRepository()
-	videoService    service.VideoService       = service.New(videoRepository)
-	loginService    service.LoginService       = service.NewLoginService()
-	jwtService      service.JWTService         = service.NewJWTService()
-
-	videoController controller.VideoController = controller.New(videoService)
-	loginController controller.LoginController = controller.NewLoginController(loginService, jwtService)
+	resourseRepo repository.ResourceRepo = repository.NewResourceRepo()
+	resourceController controller.ResourceController = controller.NewController(resourseRepo)
+	resourceApi api.ResourceApi = *api.NewResourceApi(resourceController)
 )
 
 func main() {
@@ -34,34 +30,34 @@ func main() {
 	docs.SwaggerInfo.BasePath = "/api"
 	docs.SwaggerInfo.Schemes = []string{"https"}
 
-	defer videoRepository.CloseDB()
+
+	defer resourseRepo.CloseDB()
 
 	server := gin.Default()
 
-	videoAPI := api.NewVideoAPI(loginController, videoController)
 
 	apiRoutes := server.Group(docs.SwaggerInfo.BasePath)
 	{	
 		
 		aws := apiRoutes.Group("/aws")
 		{
-			aws.GET("", videoAPI.GetVideos).Use(middlewares.Auth())
-			aws.POST("", videoAPI.CreateVideo).Use(middlewares.Auth())
-			aws.DELETE(":id", videoAPI.DeleteVideo).Use(middlewares.Auth())
+			aws.GET("", resourceApi.GetAwsResources).Use(middlewares.Auth())
+			aws.POST("", resourceApi.AddAwsResource).Use(middlewares.Auth())
+			//aws.DELETE(":id", videoAPI.DeleteVideo).Use(middlewares.Auth())
 		}
 
 		azure := apiRoutes.Group("/azure")
 		{
-			azure.GET("", videoAPI.GetVideos).Use(middlewares.Auth())
-			azure.POST("", videoAPI.CreateVideo).Use(middlewares.Auth())
-			azure.DELETE(":id", videoAPI.DeleteVideo).Use(middlewares.Auth())
+			azure.GET("", resourceApi.GetAzureResources).Use(middlewares.Auth())
+			azure.POST("", resourceApi.AddAzureResource).Use(middlewares.Auth())
+			//azure.DELETE(":id", videoAPI.DeleteVideo).Use(middlewares.Auth())
 		}
 
 		gcp := apiRoutes.Group("/gcp")
 		{
-			gcp.GET("", videoAPI.GetVideos).Use(middlewares.Auth())
-			gcp.POST("", videoAPI.CreateVideo).Use(middlewares.Auth())
-			gcp.DELETE(":id", videoAPI.DeleteVideo).Use(middlewares.Auth())
+			gcp.GET("", resourceApi.GetGcpResources).Use(middlewares.Auth())
+			gcp.POST("", resourceApi.AddGcpResource).Use(middlewares.Auth())
+			//gcp.DELETE(":id", videoAPI.DeleteVideo).Use(middlewares.Auth())
 		}
 	}
 
